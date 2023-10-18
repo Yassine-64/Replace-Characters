53ec4edbdbd5316e4fc3c01167310d9a526cdac8
Yassine mazhare <137923652+Yassine-64@users.noreply.github.com>
2023-10-18 14:39:20 +0200

    Add files via upload

---
+++ replace_char.py
@@ -0,0 +1,5 @@
+string = input("Entrer la chaine de caractere: ")
+char_to_replace = input("entrer le caractere a remplacer: ")
+new_char = input("entrer le nouveau caractere: ")
+new_string = string.replace(char_to_replace, new_char)
+print(new_string)
